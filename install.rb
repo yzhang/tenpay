3f38f107950c20d97c23d0fec482f8a72e9411ed
U-MICROSOF-2DF20B\new <new@MICROSOF-2DF20B.(none)>
2009-08-19 10:40:34 +0800

    update install.rb to puts README

--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
-# Install hook code here
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 require 'ftools'
 File.copy('config/tenpay.yml', File.expand_path(File.dirname(__FILE__) + "/../../../config/tenpay.yml"))
