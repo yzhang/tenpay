5d6db7816792afef73c3a38551b829e0e8ee1213
U-MICROSOF-2DF20B\new <new@MICROSOF-2DF20B.(none)>
2009-08-19 10:56:18 +0800

    fix install script

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
+dest_file = File.join(RAILS_ROOT, "config/tenpay.yml")
+src_file = File.join(File.dirname(__FILE__) , 'config/tenpay.yml')
+FileUtils.cp_r(src_file, dest_file)
+
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-#require 'ftools'
-#File.copy('config/tenpay.yml', File.expand_path(File.dirname(__FILE__) + "/../../../config/tenpay.yml"))
